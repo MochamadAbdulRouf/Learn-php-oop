9bbe0bdc4d15f519bc956c2f0707990f70b94132
Rouf -Myz <rouf08412@gmail.com>
2025-07-07 11:54:17 +0700

    Fix Error PHP OOP

--- data/person.php
+++ data/person.php
@@ -1,29 +1,37 @@
 <?php
 
-// Class,Object,Properties
+// Class, Object, Properties
 
 class Person
 {
+    const AUTHOR = "Mochamad Abdul Rouf"; // Constant didalam class
+
     var string $name;
     var string $address;
     var string $country = "Indonesia"; // Properties Default Value
     var ?string $truck = "null"; // Nullable Properties
     var string $mobil;
 
-// Function pada object dan mendeklarasikan function tersebut didalam blok class
+    // Function pada object dan mendeklarasikan function tersebut didalam blok class
+    /*
     function sayHello(string $name)
     {
         echo "Hello $name" . PHP_EOL;
     }
-}
+    */
+    
 
-/* This Keyword (error)
     function sayHello(?string $name)
     {
-        if (is_null($name1)) {
-            echo "Hi, my name is $this->name1" . PHP_EOL;
+        if (is_null($name)) {
+            echo "Hi, my name is $this->name" . PHP_EOL;
         } else {
-            echo "Hi $name1, my name is $this->name1" . PHP_EOL;
+            echo "Hi $name, my name is $this->name" . PHP_EOL;
         }
     }
-*/
+
+    function info()
+    {
+        echo "Author : " . person::AUTHOR . PHP_EOL;
+    }
+}
