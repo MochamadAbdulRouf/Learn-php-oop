bf55fe5ccae14a3694425ab36ab0cf579fd15eb0
Rouf -Myz <rouf08412@gmail.com>
2025-07-08 14:47:33 +0700

    Commit global namespace php oop

---
+++ global-namespace.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace {
+    require_once "data/conflict.php";
+    require_once "data/helper.php";
+
+    $conflict1 = new Data\One\Conflict();
+    $conflict2 = new Data\Two\Conflict();
+
+    // helper function dan constant namespace global
+    echo Helper\APPLICATION . PHP_EOL;
+    Helper\helpMe();   
+}
