1b8cbe4b954bb1fa8c34765b940ddc670d9fcdee
Rouf -Myz <rouf08412@gmail.com>
2025-07-15 08:52:58 +0700

    Update My Learn

--- data/animal.php
+++ data/animal.php
@@ -4,11 +4,16 @@
 
 namespace Data;
 
+// require ke function contravariance
+require_once "food.php";
+
 abstract class Animal
 {
     public string $name;
 
     abstract public function run(): void;
+
+    abstract public function eat(AnimalFood $animalFood): void;
 }
 
 class Cat extends Animal 
@@ -17,6 +22,12 @@ class Cat extends Animal
     {
         echo "Cat $this->name is running" . PHP_EOL;
     }
+
+    
+    public function eat(AnimalFood $animalFood): void
+    {
+        echo "Cat is eating" . PHP_EOL;
+    }
 }
 
 class Dog extends Animal
@@ -24,5 +35,12 @@ class Dog extends Animal
     public function run(): void 
     {
         echo "Dog $this->name is running" . PHP_EOL;
+    }
+
+    
+    // contravariance. dari child kita jadikan parent
+    public function eat(Food $animalFood): void
+    {
+        echo "Dog is eating" . PHP_EOL;
     }
 }
